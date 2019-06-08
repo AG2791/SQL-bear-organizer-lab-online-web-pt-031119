f542236c5c9243b07b206854e59b25191348e4f0
AJ Guiles <ashiguiles@gmail.com>
2019-06-08 22:25:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,17 +1,17 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
 VALUES 
-(1,"Mr. Chocolate",10, "male",Brown, "kind", 1),
+(1,"Mr. Chocolate",10, "male",Brown, "kind", 1);
 
-(2,"Rowdy",10, "male",Brown, "kind", 1),
+(2,"Rowdy",10, "male",Brown, "kind", 1);
 
-(3,"Tabitha",10, "female",Brown, "kind", 1),
+(3,"Tabitha",10, "female",Brown, "kind", 1);
  
-(4,"Seargent Brown",10, "male",Brown, "kind", 1),
+(4,"Seargent Brown",10, "male",Brown, "kind", 1);
 
-(5,"Melissa",10, "female",Brown, "kind", 1),
+(5,"Melissa",10, "female",Brown, "kind", 1);
 
-(6,"Grinch",10, "male",Brown, "kind", 1),
+(6,"Grinch",10, "male",Brown, "kind", 1);
  
-(7,"Wendy",10, "female",Brown, "kind", 1),
+(7,"Wendy",10, "female",Brown, "kind", 1);
 
-(8,NULL,10, "male",Brown, "kind", 1),
+(8,NULL,10, "male",Brown, "kind", 1)
